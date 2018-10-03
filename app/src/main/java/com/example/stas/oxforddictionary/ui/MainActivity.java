afd437731ca4216fc13291372e064648eb06bfb2
Stas Stahma <StasStahma@gmail.com>
2018-10-03 21:31:08 +0300

    navigation drawer implemented

--- app/src/main/java/com/example/stas/oxforddictionary/ui/MainActivity.java
+++ app/src/main/java/com/example/stas/oxforddictionary/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.stas.oxforddictionary.ui;
 
 import android.support.annotation.NonNull;
+import android.support.constraint.Group;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,19 +10,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import com.example.stas.oxforddictionary.R;
 import java.util.Objects;
+
+import butterknife.BindBitmap;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity implements MainContract.View{
 
-    @BindView(R.id.output) TextView output;
+
     @BindView(R.id.nav_view) NavigationView navigationView;
     @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
-    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.hamburgerBtn)ImageButton hamburgerBtn;
 
     private MainContract.Presenter presenter;
 
@@ -32,33 +38,23 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         ButterKnife.bind(this);
         presenter = new MainPresenter(this);
 
-
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        Objects.requireNonNull(actionBar).setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 menuItem.setChecked(true);
                 drawerLayout.closeDrawers();
+                //todo implement swaping fragments
                 return true;
             }
         });
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void showWord(String word) {
+
     }
 
-    @Override
-    public void showWord(String word) {
-        output.setText(word);
+    public void openDrawer(View view) {
+        drawerLayout.openDrawer(GravityCompat.START);
     }
 }
