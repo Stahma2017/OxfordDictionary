833acd623e7458e2df8da1c92d6280dd79c7b19e
Stas Stahma <StasStahma@gmail.com>
2018-10-04 00:52:23 +0300

    Hamburger icon fixed

--- app/src/main/java/com/example/stas/oxforddictionary/ui/MainActivity.java
+++ app/src/main/java/com/example/stas/oxforddictionary/ui/MainActivity.java
@@ -1,23 +1,17 @@
 package com.example.stas.oxforddictionary.ui;
 
 import android.support.annotation.NonNull;
-import android.support.constraint.Group;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import com.example.stas.oxforddictionary.R;
-import java.util.Objects;
-
-import butterknife.BindBitmap;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -37,6 +31,10 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         presenter = new MainPresenter(this);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        EntryFragment entryFragment = new EntryFragment();
+        fragmentTransaction.add(R.id.content_frame, entryFragment);
+        fragmentTransaction.commit();
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
